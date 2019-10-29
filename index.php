6cb3e2cf379f27b37edd25bc52539ba80f95e99e
bunjabi <inija.martina@gmail.com>
2019-10-29 17:32:00 +0200

    uzduotis 5

--- index.php
+++ index.php
@@ -7,6 +7,6 @@
       <h1> Dainu tekstai</h1>
       <h2> Paragrafo daina</h2>
       <p> As toks vienisas jaunas paragrafas...</p>
-      <p> Mindau svetimo kodo gatves...</p>
+      /* <p> Mindau svetimo kodo gatves...</p> */
   </body>
 </html>
