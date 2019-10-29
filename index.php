983790bbe7ac78c418cfa6cf168ce87bcbd176ec
bunjabi <inija.martina@gmail.com>
2019-10-29 16:53:16 +0200

    uzduotis 3

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title> <?php print 'Að,'. date($format='l') . print 'ir PHP' ?> </title>
+    <title> <?php print 'Að,'. date('l') . print 'ir PHP' ?> </title>
   </head>
   <body>
-      <h1>Inija-HTML <?php print 'ir PHP' ?> asë jau nuo <?php date($format='Y') . print 'metø'?></h1>
-    <p>Viskas prasidëjo <?php date($format='F') . print 'mënesio' . date($format='d') . print 'dienà!'?></p>
+      <h1>Inija-HTML <?php print 'ir PHP' ?> asë jau nuo <?php date('Y') . print 'metø'?></h1>
+    <p>Viskas prasidëjo <?php date('F') . print 'mënesio' . date('d') . print 'dienà!'?></p>
   </body>
 </html>
