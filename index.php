b582ad332e513547abb659705426bf78b82d1dad
bunjabi <inija.martina@gmail.com>
2019-10-29 16:05:08 +0200

    uzduotis 2

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <html>
   <head>
-    <title>Aš valdau HTML</title>
+    <title>Aš valdau HTML <?php print 'ir PHP' </title>
   </head>
   <body>
-    <h1>Inija-HTML asė!</h1>
+    <h1>Inija-HTML <?php print 'ir PHP' ?> asė!</h1>
     <p>Kuriu dinaminį turinį. Atidaryta:</p> <?php date($time) ?>
   </body>
 </html>
