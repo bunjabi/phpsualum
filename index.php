50a93b91e1a1c357360661a89fa2e831191baa2d
bunjabi <inija.martina@gmail.com>
2019-10-29 17:26:42 +0200

    uzduotis 5

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title> PHP lydës ir <?php print date('Y-m-d', strtotime('+1 day') ) ; ?> </title>
+    <title> Komentarai</title>
   </head>
   <body>
-      <h1><b>Inija</b>- PHP su mainm buvo ir <?php print date('H', strtotime('- 1 hour')); ?></h1>
-    <p> <?php print date('Y', strtotime('+1 year'))?> ne uþ kalnø</p>
+      <h1> Dainu tekstai</h1>
+      <h2> Paragrafo daina</h2>
+      <p> As toks vienisas jaunas paragrafas...</p>
+      <p> Mindau svetimo kodo gatves...</p>
   </body>
 </html>
