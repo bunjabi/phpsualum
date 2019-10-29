266104b8ced8220ee753ea7af8447654d5ea4e26
bunjabi <inija.martina@gmail.com>
2019-10-29 17:51:46 +0200

    uzd 7

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title> PHP lydës ir <?php print date('Y-m-d', strtotime('+' . rand(0,10) . 'year')</title>
+    <title> PHP lydës ir <?php print date('Y-m-d', strtotime('+' . rand(0,10) . 'year';) ?></title>
   </head>
   <body>
       <h1>
             <b>Inija</b> - Galbût turësiu <?php print rand(1,5); ?> vaikus!
       </h1>
-      <p> D. Trumpas nebebus prezidentu:<?php print date('Y-m-d'. strtotime('+' . rand(2,10) . 'years'))   </p>
+      <p> D. Trumpas nebebus prezidentu:<?php print date('Y-m-d'. strtotime('+' . rand(2,10) . 'years')); ?>  </p>
      
   </body>
 </html>
