2e3db4e930693a89f34d030763ab8fbf57c28dba
bunjabi <inija.martina@gmail.com>
2019-10-29 15:55:55 +0200

    praktika

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
-
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>AÅ¡ valdau HTML</title>
+    <title>Að valdau HTML</title> 
   </head>
   <body>
-    <h1>Inija-HTML asÄ—!</h1>
-    <p>Moku kurti statinÄ¯ turinÄ¯.</p>
+    <h1>Inija-HTML asë!</h1>
+    <p>Kuriu dinaminá turiná. Atidaryta:</p> <?php date($time) ?>
   </body>
 </html>
