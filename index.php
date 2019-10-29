c24ecd0ac6b6e97f092fcb8273b94dc90814baa3
bunjabi <inija.martina@gmail.com>
2019-10-29 15:02:44 +0200

    uzduotis 1

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
-<php?
-print 'veikia';
->
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Aš valdau HTML</title>
+  </head>
+  <body>
+    <h1>Inija-HTML asė!</h1>
+    <p>Moku kurti statinį turinį.</p>
+  </body>
+</html>
