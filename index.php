f915e49a45cf6ed086bff1916ab1cdb27102894e
bunjabi <inija.martina@gmail.com>
2019-10-30 17:14:54 +0200

    uzd 11

--- index.php
+++ index.php
@@ -2,15 +2,29 @@
   <head>
     <meta charset="utf-8">
     <title></title>
-    <style>
-        .kubas{
-            background-image: url("images\dice-<?php print rand(1,6) . '.png';?>");
-            width: 200px;
-            height: 200px;
-        }
-    </style>
+      <style>
+          .body {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+         }
+          .bomba {
+              height: <?php print date('1s'); ?>px;
+              width: <?php print date('1s'); ?>px;
+              background-image: url("https://images-na.ssl-images-amazon.com/images/I/61V2cX4JqGL.png");
+              background-size: cover;
+          }
+          .boom-00 {
+              background-image: url("https://image.businessinsider.com/5673f88bdd089539748b45e8?width=1100&format=jpeg&auto=webp");
+              background-size: cover;
+          }
+          p{
+              text-align: center;
+          }
+      </style>
   </head>
   <body>
-    <div class="kubas"></div>
+    <div class="bomba boom-<?php print date('s');?>"></div>
+    <p><?php print date('s') ?></p>
   </body>
 </html>
