074bc1d2153aafeb2a43f3087d6872ba6387dee6
bunjabi <inija.martina@gmail.com>
2019-10-31 17:51:50 +0200

    uzd 16

--- index.php
+++ index.php
@@ -7,10 +7,9 @@ $fuel_total = round(($distance / 100 * $consumption), 2);
 $price_trip = round(($fuel_total * $price_l), 2);
 $my_money = 100;
 $uzteks = '';
-if ($fuel_total > $my_money){
+if ($fuel_total > $my_money) {
     $uzteks = 'neužteks';
-}
-else{
+} else {
     $uzteks = 'užteks';
 }
 $li_1 = "Nuvažiuota distancija: $distance";
@@ -18,8 +17,6 @@ $li_2 = "Sunaudota $fuel_total litrai kuro";
 $li_3 = "Sumokėta už kurą $price_trip pinigai";
 $li_4 = "Turimi pinigai: $my_money";
 $li_5 = "Išvada: kelionei $uzteks";
-
-
 
 ?>
 <html lang="en" dir="ltr">
@@ -34,9 +31,7 @@ $li_5 = "Išvada: kelionei $uzteks";
     <li><?php print $li_4 ?></li>
     <li><?php print $li_2 ?></li>
     <li><?php print $li_3 ?></li>
-
     <hr>
-
     <p><?php print $li_5; ?></p>
 </ul>
 </body>
