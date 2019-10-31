eec5dfa616d24844932841f79461c9d6117ff1a0
bunjabi <inija.martina@gmail.com>
2019-10-31 14:59:53 +0200

    uzd 12

--- index.php
+++ index.php
@@ -1,30 +1,20 @@
+<?php
+    $li_1= 'sveiki';
+    $li_2= 123;
+    $li_3= true;
+    $li_4= null;
+ ?>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
     <title></title>
-      <style>
-          .body {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-         }
-          .bomba {
-              height: <?php print date('1s'); ?>px;
-              width: <?php print date('1s'); ?>px;
-              background-image: url("https://images-na.ssl-images-amazon.com/images/I/61V2cX4JqGL.png");
-              background-size: cover;
-          }
-          .boom-00 {
-              background-image: url("https://image.businessinsider.com/5673f88bdd089539748b45e8?width=1100&format=jpeg&auto=webp");
-              background-size: cover;
-          }
-          p{
-              text-align: center;
-          }
-      </style>
   </head>
   <body>
-    <div class="bomba boom-<?php print date('s');?>"></div>
-    <p><?php print date('s') ?></p>
+    <ul>
+        <li><?php print $li_1;?></li>
+        <li><?php print $li_2;?></li>
+        <li><?php print $li_3;?></li>
+        <li><?php print $li_4;?></li>
+    </ul>
   </body>
 </html>
