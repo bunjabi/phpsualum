4faefd80e39225bb459af32680c4c21d7d858d12
bunjabi <inija.martina@gmail.com>
2019-10-31 15:54:41 +0200

    uzd 14

--- index.php
+++ index.php
@@ -1,15 +1,17 @@
 <?php
-   $img='images/suo.jpg'
+   $h1_skola = (rand(1,100));
+   $h3_1kab = (rand(101,200));
+   $h3_2kab = (rand(201,300));
  ?>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Suo</title>
+    <title>Skolos Skaiciuokle</title>
   </head>
   <body>
-    <img src="<?php print $img; ?>">
-    <img src="<?php print $img; ?>">
-    <img src="<?php print $img; ?>">
-    <img src="<?php print $img; ?>">
+    <h1>Skolos skaiciuokle</h1>
+    <h3>Jei paėmei <?php print $h1_skola;?> jievrų</h3>
+    <h3>Su dviem kabančiais grąžinsi <?php print $h3_2kab;?></h3>
+    <h3>Su vienu kabančiu grąžinsi <?php print $h3_1kab;?></h3>
   </body>
 </html>
