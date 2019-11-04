5efbf0f14d6095eb4d76baf7c6a823025c8b490d
bunjabi <inija.martina@gmail.com>
2019-11-04 10:41:27 +0200

    21

--- index.php
+++ index.php
@@ -1,36 +1,30 @@
 <?php
-$h2_1='Grizai velai';
-$h2_2='Grizai velai ir isgeres';
-$h2_3='Grizai isgeres';
-$h2_4='Nieko nepadare';
-$h2=null;
+    $h2_1='Grizai velai';
+    $h2_2='Grizai velai ir isgeres';
+    $h2_3='Grizai isgeres';
+    $h2_4='Nieko nepadare';
+    $h2=null;
+    $grizai_velai= rand(0,1);
+    $grizai_isgeres=rand(0,1);
 
-$grizai_velai= rand(0,1);
-$grizai_isgeres=rand(0,1);
-
-if ($grizai_isgeres){
-    if ($grizai_velai){
-        $h2= $h2_2;
+    if ($grizai_isgeres){
+        if ($grizai_velai){
+            $h2= $h2_2;
+        }
+        $h2=$h2_3;
     }
-    $h2=$h2_3;
-}
-elseif($grizai_velai){
-    $h2=$h2_1;
-}
-else{
-    $h2=$h2_4;
-}
+    elseif($grizai_velai){
+        $h2=$h2_1;
+    }
+    else{
+        $h2=$h2_4;
+    }
 
 ?>
 
 <html lang="en" dir="ltr">
-<head>
-
-</head>
 <body>
     <h1> Buitine skaiciuokle </h1>
     <h2><?php print $h2; ?> </h2>
-
-
 </body>
 </html>
