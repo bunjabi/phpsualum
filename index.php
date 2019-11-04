60bc0c3093082a0c931b7ba0febc661989085d6e
bunjabi <inija.martina@gmail.com>
2019-11-04 11:53:36 +0200

    23

--- index.php
+++ index.php
@@ -1,28 +1,42 @@
 <?php
 
-    $h2 = null;
-    $grizai_velai = rand(0, 1);
-    $grizai_isgeres = rand(0, 1);
-    $h3_miegosi = 'nemiegosi';
-    $h3 = "$h3_miegosi ant sofos";
-
-    if ($grizai_isgeres) {
-        if ($grizai_velai) {
-            $h2 = 'Grizai velai ir isgeres';
-            $h3_miegosi = 'miegosi;';
-        }
-        $h2 = 'grizai isgeres';
-    } elseif ($grizai_velai) {
-        $h2 = 'Grizai velai';
+    $sunny = rand(0, 1);
+    $orai = ['Debesuota', 'Sauleta'];
+    if ($sunny) {
+        $oras = $orai[1];
+        $style = 'sauleta';
     } else {
-        $h2 = 'Nieko nepadarei';
+        $oras = $orai[0];
+        $style = 'debesuota';
     }
 
 ?>
 <html lang="en" dir="ltr">
+<head>
+    <style>
+        div {
+            width: 300px;
+            height: 300px;
+            background-position: center;
+        }
+
+        .sauleta {
+            background-image: url("https://i.dlpng.com/static/png/1397265-chocolate-melting-in-the-sun-melting-sun-png-300_250_preview.png");
+        }
+
+        .debesuota {
+            background-image: url("https://cdn.drawception.com/images/panels/2018/1-7/wL99Mkqwh1-2.png");
+        }
+
+        body {
+            display: flex;
+            justify-content: flex-start;
+            align-items: center;
+        }
+    </style>
+</head>
 <body>
-    <h1> Buitine skaiciuokle </h1>
-    <h2><?php print $h2; ?> </h2>
-    <h3><?php print $h3; ?></h3>
+<div class="<?php print $style; ?>"></div>
+<p><?php print $oras; ?></p>
 </body>
 </html>
