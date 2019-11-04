c0e7f7778f12c2fb5fce2898797e4abc2b01aafb
bunjabi <inija.martina@gmail.com>
2019-11-04 11:00:12 +0200

    uzd 22

--- index.php
+++ index.php
@@ -6,25 +6,28 @@
     $h2=null;
     $grizai_velai= rand(0,1);
     $grizai_isgeres=rand(0,1);
+    $h3_miegosi='nemiegosi';
+    $h3="$h3_miegosi ant sofos";
 
     if ($grizai_isgeres){
         if ($grizai_velai){
-            $h2= $h2_2;
+            $h2= 'Grizai velai ir isgeres';
+            $h3_miegosi='miegosi;';
         }
-        $h2=$h2_3;
+        $h2='grizai isgeres';
     }
     elseif($grizai_velai){
-        $h2=$h2_1;
+        $h2='Grizai velai';
     }
     else{
-        $h2=$h2_4;
+        $h2='Nieko nepadarei';
     }
 
 ?>
-
 <html lang="en" dir="ltr">
 <body>
     <h1> Buitine skaiciuokle </h1>
     <h2><?php print $h2; ?> </h2>
+    <h3><?php print $h3; ?></h3>
 </body>
 </html>
