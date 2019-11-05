bca604112c36a5d449f18c5185fb1efea41bb63b
bunjabi <inija.martina@gmail.com>
2019-11-05 10:58:44 +0200

    uzd 32

--- index.php
+++ index.php
@@ -1,15 +1,26 @@
 <?php
 
-    $x = rand(0, 20);
-    $p = null;
-
-    for ($i = 0; $i < $x; $i++) {
-        $p = "Tai yra $i-tasis ciklas";
-        var_dump($i);
+    $kates = rand(1, 3);
+    $sunys = rand(1, 3);
+    $katasuniai = 0;
+    $pavyko = null;
+    for ($i = 0; $i < $kates; $i++) {
+        for ($x = 0; $x < $sunys; $x++) {
+            $pavyko = rand(0, 1);
+            if ($pavyko) {
+                $katasuniai++;
+                break;
+            }
+        }
     }
+    $h2 = "Dalyvavo $kates kates ir $sunys sunys.";
+    $h3 = "katsuniu iseiga: $katasuniai";
 
 ?>
 <html lang="en" dir="ltr">
     <body>
+        <h1> Katasuniu Iseiga</h1>
+        <h2><?php print $h2; ?></h2>
+        <h3><?php print $h3; ?></h3>
     </body>
 </html>
