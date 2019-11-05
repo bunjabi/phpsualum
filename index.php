9bf9150c153c48c4881559fc714b08ba059b13f7
bunjabi <inija.martina@gmail.com>
2019-11-05 10:10:13 +0200

    31

--- index.php
+++ index.php
@@ -1,41 +1,15 @@
 <?php
 
-    $bool = true;
-    $str = "1";
-    $int = 1;
-    $flt = 1.23;
-    $str_1 = "1.23";
-    $equals = null;
-    $li_1="Bool (true) ir Integer (1):$equals";
-    $li_2="String (1) ir Boolean (1):$equals";
-    $li_3="Float (1.23) ir String (1.23):$equals";
+    $x = rand(0, 20);
+    $p = null;
 
-    if ($bool === $int) {
-        $equals = 'Identiski';
-    }
-    elseif ($bool == $int) {
-        $equals = 'Lygus';
-    }
-    if ($str === $bool) {
-        $equals = 'Identiski';
-    }
-    elseif ($str == $bool) {
-        $equals = 'Lygus';
-    }
-    if ($flt === $str_1) {
-        $equals = 'Identiski';
-    }
-    elseif ($flt == $str_1) {
-        $equals = 'Lygus';
+    for ($i = 0; $i < $x; $i++) {
+        $p = "Tai yra $i-tasis ciklas";
+        var_dump($i);
     }
 
 ?>
 <html lang="en" dir="ltr">
     <body>
-        <ul>
-            <li><?php print $li_1; ?></li>
-            <li><?php print $li_2; ?></li>
-            <li><?php print $li_3; ?></li>
-        </ul>
     </body>
 </html>
