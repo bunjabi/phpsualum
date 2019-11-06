852b488fb002fd0f374004b3f3d3d6fbcf97e17c
bunjabi <inija.martina@gmail.com>
2019-11-06 12:23:28 +0200

    uzd 4.4

--- index.php
+++ index.php
@@ -2,16 +2,18 @@
 
 $car_price_new = 30000;
 $car_price_used = $car_price_new;
-$months = 24;
 $depreciation = 0.02;
+$santaupos = 15000;
 
-for ($i = 0; $i < $months; $i++) {
-    $car_price_used -= round($car_price_used * 0.02, 2);
+for ($i = 0; $car_price_used > $santaupos; $i++) {
+    $car_price_used *= 0.98;
 }
 
-$depr_prec = 100 - ($car_price_used / $car_price_new * 100);
+$car_price_used = round($car_price_used, 2);
+$months = $i;
+$depr_prec = round(100 - ($car_price_used / $car_price_new * 100), 2);
 $h2 = "Naujos masinos kaina $car_price_new";
-$h3 = "po $months men, masinos verte bus: $car_price_used eur";
+$h3 = "Masina galesi nusipirkti po $months menesiu, kai jos verte bus $car_price_used";
 $h4 = "Masina nuevertes $depr_prec proc."
 
 ?>
